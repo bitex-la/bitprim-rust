9ce8ef83e00d177bf9a99b36005ae3cbc2b3c7f6
Werner Echezuria <werner_a_e@yahoo.es>
2018-05-04 17:34:39 -0300

    if version specified by BITPRIM_VERSION does not exists, choose the last one

--- install_vendor.sh
+++ install_vendor.sh
@@ -8,6 +8,15 @@ rm deploy_manifest.txt
 for file in vendor/bitprim_btc/*
 do
 	file_name="${file##*/}"
-	folder="${file_name:3:-2}"
-	cp "$(find ~/.conan/data/$folder/$BITPRIM_VERSION -name $file_name)" vendor/bitprim_btc/${file##*/}
+	folder=~/.conan/data/${file_name:3:-2}
+	if [ -e  $folder ]
+	then
+		if [ -e  $folder/$BITPRIM_VERSION ]
+		then
+			cp "$(find $folder/$BITPRIM_VERSION -name $file_name)" vendor/bitprim_btc/${file##*/}
+		else
+			last_version="$(ls -1 $folder | tail -n 1)"
+			cp "$(find $folder/$last_version -name $file_name)" vendor/bitprim_btc/${file##*/}
+		fi
+	fi
 done
