20130009c0c6949950b1cf48292ae0e99aab249a
Werner Echezuria <werner_a_e@yahoo.es>
2018-05-04 14:33:47 -0300

    get correct folder and file name

--- install_vendor.sh
+++ install_vendor.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Should be last version
 BITPRIM_VERSION=0.9
 conan install bitprim-node-exe/$BITPRIM_VERSION@bitprim/stable -o currency=BTC
 rm bn
@@ -8,6 +7,7 @@ rm deploy_manifest.txt
 
 for file in vendor/bitprim_btc/*
 do
-	path_file="$(find ~/.conan/data/bitprim-blockchain/$BITPRIM_VERSION -name "${file##*/}")"
-	cp $path_file vendor/bitprim_btc/${file##*/}
+	file_name="${file##*/}"
+	folder="${file_name:3:-2}"
+	cp "$(find ~/.conan/data/$folder/$BITPRIM_VERSION -name $file_name)" vendor/bitprim_btc/${file##*/}
 done
