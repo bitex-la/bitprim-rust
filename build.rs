c81c05b99e3d9627f7c32fe36692debd8e11658e
Werner Echezuria <werner_a_e@yahoo.es>
2018-05-07 18:18:42 -0300

    allow the use of features to compile against a currency

--- build.rs
+++ build.rs
@@ -1,23 +1,29 @@
 extern crate regex;
 use std::fs;
 use regex::Regex;
-use std::env;
 
 fn main(){
-  println!(r"cargo:rustc-link-search=/usr/lib/gcc/x86_64-linux-gnu/7");
-  println!(r"cargo:rustc-link-lib=static=stdc++");
+    println!(r"cargo:rustc-link-search=/usr/lib/gcc/x86_64-linux-gnu/7");
+    println!(r"cargo:rustc-link-lib=static=stdc++");
 
-  let currency_target = env::var("CURRENCY_TARGET").unwrap_or("BCH".to_string());
-  let libs = ["./vendor/bitprim_", &currency_target.to_lowercase()].join("");
-  println!(r"cargo:rustc-link-search={}", libs);
+    #[allow(unused_variables)]
+    let currency_target = "bch";
 
-  let re = Regex::new(r"lib([0-9A-Za-z_-]+)\.a").unwrap();
-  let paths = fs::read_dir(libs).expect("Path not found");
+    #[cfg(feature = "btc")]
+    let currency_target = "btc";
+    #[cfg(feature = "ltc")]
+    let currency_target = "ltc";
 
-  for entry in paths {
-    let path = entry.unwrap().path();
-    let filename = path.to_str().unwrap();
-    let captures = re.captures(filename).expect("A non-library found");
-    println!(r"cargo:rustc-link-lib=static={}", &captures[1]);
-  }
+    let libs = ["./vendor/bitprim_", currency_target].join("");
+    println!(r"cargo:rustc-link-search={}", libs);
+
+    let re = Regex::new(r"lib([0-9A-Za-z_-]+)\.a").unwrap();
+    let paths = fs::read_dir(libs).expect("Path not found");
+
+    for entry in paths {
+        let path = entry.unwrap().path();
+        let filename = path.to_str().unwrap();
+        let captures = re.captures(filename).expect("A non-library found");
+        println!(r"cargo:rustc-link-lib=static={}", &captures[1]);
+    }
 }
