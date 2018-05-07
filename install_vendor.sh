8d3673debd4ac64cf27716abbb59ea276d594e08
Werner Echezuria <werner_a_e@yahoo.es>
2018-05-07 15:18:52 -0300

    sets currency argument to install_vendor

--- install_vendor.sh
+++ install_vendor.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
+if [ -z "$1" ] ; then
 	echo 'You need to pass a version number'
 	exit 0
 fi
 BITPRIM_VERSION=$1
 
-folders=(bitprim-blockchain bitprim-consensus bitprim-core bitprim-database bitprim-network 
+if [ -z "$2" ] ; then
+	echo 'You need to pass a currency'
+	exit 0
+fi
+CURRENCY=$2
+
+FOLDERS=(bitprim-blockchain bitprim-consensus bitprim-core bitprim-database bitprim-network 
 bitprim-node-cint bitprim-node boost gmp icu secp256k1)
-files=(libbitprim-blockchain.a libbitprim-consensus.a libbitprim-core.a libbitprim-database.a
+FILES=(libbitprim-blockchain.a libbitprim-consensus.a libbitprim-core.a libbitprim-database.a
 libbitprim-network.a libbitprim-node-cint.a libbitprim-node.a libboost_filesystem.a
 libboost_iostreams.a libboost_log.a libboost_program_options.a libboost_regex.a
 libboost_system.a libboost_thread.a libgmp.a libsecp256k1.a)
 
-conan install bitprim-node-exe/$BITPRIM_VERSION@bitprim/stable -o currency=BTC
-conan install bitprim-node-cint/$BITPRIM_VERSION@bitprim/stable -o currency=BTC
+conan install bitprim-node-exe/$BITPRIM_VERSION@bitprim/stable -o currency=${CURRENCY^^}
+conan install bitprim-node-cint/$BITPRIM_VERSION@bitprim/stable -o currency=${CURRENCY^^}
 rm bn
 rm deploy_manifest.txt
 
+mkdir -p vendor/bitprim_${CURRENCY,,}
+
 # ex. find_files folder
 function find_files {
-	for file in ${files[*]}
+	for file in ${FILES[*]}
 	do
 		found=$(find $1 -name $file -print -quit)
 		if [[ $found ]]
 		then
-			cp $found vendor/bitprim_btc/$file
+			cp $found vendor/bitprim_${CURRENCY,,}/$file
 		fi
 	done
 }
 
-for folder in ${folders[*]}
+for folder in ${FOLDERS[*]}
 do
 	package_path=~/.conan/data/$folder
 	if [ -e  $package_path ]
