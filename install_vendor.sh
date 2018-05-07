c9aaa47a5252c97f04a03d0f5fdcc03d574c2fd9
Werner Echezuria <werner_a_e@yahoo.es>
2018-05-07 14:38:30 -0300

    if file exists performs copy

--- install_vendor.sh
+++ install_vendor.sh
@@ -4,6 +4,7 @@ if [[ $# -eq 0 ]] ; then
 	echo 'You need to pass a version number'
 	exit 0
 fi
+BITPRIM_VERSION=$1
 
 folders=(bitprim-blockchain bitprim-consensus bitprim-core bitprim-database bitprim-network 
 bitprim-node-cint bitprim-node boost gmp icu secp256k1)
@@ -12,7 +13,6 @@ libbitprim-network.a libbitprim-node-cint.a libbitprim-node.a libboost_filesyste
 libboost_iostreams.a libboost_log.a libboost_program_options.a libboost_regex.a
 libboost_system.a libboost_thread.a libgmp.a libsecp256k1.a)
 
-BITPRIM_VERSION=$1
 conan install bitprim-node-exe/$BITPRIM_VERSION@bitprim/stable -o currency=BTC
 conan install bitprim-node-cint/$BITPRIM_VERSION@bitprim/stable -o currency=BTC
 rm bn
@@ -22,22 +22,25 @@ rm deploy_manifest.txt
 function find_files {
 	for file in ${files[*]}
 	do
-		cp "$(find $1 -name $file)" vendor/bitprim_btc/$file
+		found=$(find $1 -name $file)
+		if [[ $found ]]
+		then
+			cp $found vendor/bitprim_btc/$file
+		fi
 	done
 }
 
 for folder in ${folders[*]}
 do
-	file_name="${file##*/}"
-	dir=~/.conan/data/$folder
-	if [ -e  $folder ]
+	package_path=~/.conan/data/$folder
+	if [ -e  $package_path ]
 	then
-		if [ -e  $folder/$BITPRIM_VERSION ]
+		if [ -e  $package_path/$BITPRIM_VERSION ]
 		then
-			find_files $folder/$BITPRIM_VERSION
+			find_files $package_path/$BITPRIM_VERSION
 		else
-			last_version="$(ls -1 $folder | tail -n 1)"
-			find_files $folder/$last_version
+			last_version="$(ls -1 $package_path | tail -n 1)"
+			find_files $package_path/$last_version
 		fi
 	fi
 done
