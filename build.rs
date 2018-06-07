9dd40f4c3c0063293edbc62f5208f6daca9cd2b0
werner <werner@yahoo.es>
2018-06-07 12:03:55 -0300

    Return build.rs to the original state

--- build.rs
+++ build.rs
@@ -6,15 +6,16 @@ fn main() {
     println!(r"cargo:rustc-link-search=/usr/lib/gcc/x86_64-linux-gnu/7");
     println!(r"cargo:rustc-link-lib=static=stdc++");
 
+    let libs = "./vendor/bitprim_btc";
+    println!(r"cargo:rustc-link-search={}", libs);
+
     let re = Regex::new(r"lib([0-9A-Za-z_-]+)\.a").unwrap();
-    for lib in ["./vendor/bitprim_btc", "./vendor/bitprim_bch"].iter() {
-        println!(r"cargo:rustc-link-search={}", lib);
-        let path = fs::read_dir(lib).expect("Path not found");
-        for entry in path {
-            let path = entry.unwrap().path();
-            let filename = path.to_str().unwrap();
-            let captures = re.captures(filename).expect("A non-library found");
-            println!(r"cargo:rustc-link-lib=static={}", &captures[1]);
-        }
+    let paths = fs::read_dir(libs).expect("Path not found");
+
+    for entry in paths {
+        let path = entry.unwrap().path();
+        let filename = path.to_str().unwrap();
+        let captures = re.captures(filename).expect("A non-library found");
+        println!(r"cargo:rustc-link-lib=static={}", &captures[1]);
     }
 }
